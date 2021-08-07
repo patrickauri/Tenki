688b7245930d5973e350b02b13b109e796563e65
Patrick Auri <patrickauri@nthree.io>
2021-08-07 12:35:04 +0200

    Added dotenv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+require('dotenv').config()
+
 const App = () => {
-    return 'App'
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(process.env.REACT_APP_API_KEY)
+    }
+
+    return (
+        <main>
+            <h1>Tenki</h1>
+            <section>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <input type='text' placeholder='Enter a location'></input>
+                    <button>Search</button>
+                </form>
+            </section>
+        </main>
+    )
 }
 
 export default App
